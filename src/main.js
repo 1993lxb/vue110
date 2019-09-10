6471b6187c9c0bbe26369d3a261b7a6352925437
1993lxb <36787012+1993lxb@users.noreply.github.com>
2019-09-10 16:43:55 +0800

    add homecontainer

--- src/main.js
+++ src/main.js
@@ -3,17 +3,32 @@ console.log('OK')
 
 import Vue from 'vue'
 
-import { Header } from 'mint-ui';
+import vueRouter from 'vue-router'
+
+Vue.use(vueRouter)
+
+import { Header,Swipe, SwipeItem } from 'mint-ui';
 
 import './lib/mui/css/mui.css'
 
+import './lib/mui/css/icons-extra.css'
+
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 
 import app from './App.vue'
 
+import router from './router.js'
+
+
+import vueResource from 'vue-resource'
+Vue.use(vueResource)
+
 
 var vm=new Vue({
     el : '#app',
-    render : c => c(app)
+    render : c => c(app),
+    router
 })
