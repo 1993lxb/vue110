19613b93c031eeb19464803d0691bbb2e4874b28
1993lxb <36787012+1993lxb@users.noreply.github.com>
2019-10-16 17:47:08 +0800

    add in 20191016

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
-
+import NewsList from './components/news/newslist.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -14,7 +14,8 @@ var router = new VueRouter({
     {path : '/member' , component : MemberContainer},
     {path : '/shopcar' , component : ShopcarContainer},
     {path : '/search' , component : SearchContainer},
-    {path : '/' , redirect : '/home'}
+    {path : '/' , redirect : '/home'},
+    {path : '/home/newlist', component : NewsList}
   ],
   linkActiveClass : 'mui-active'
 })
